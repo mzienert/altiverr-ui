ac7711183c7df244583fc7d9e11402d32cd1020a
Matthew Zienert <mzienert@gmail.com>
2025-04-11 14:21:26 -0600

    add suspense boundary

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { analytics } from "@/lib/analytics";
 import type { Metadata, Viewport } from "next";
 import { Inter as FontSans } from "next/font/google";
 import Analytics from "@/components/analytics";
+import { Suspense } from "react";
 import "./globals.css";
 
 const GA_MEASUREMENT_ID = 'G-D6D70ETZSV';
@@ -42,7 +43,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <Analytics />
+        <Suspense>
+          <Analytics />
+        </Suspense>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
